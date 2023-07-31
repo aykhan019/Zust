23147f2c5f955201d1f30e03c3661a3ead97dbf1
Aykhan <ayxanx17@gmail.com>
2023-07-31 23:16:19 +0400

    Added summaries to files in folder "Abstract" in Zust.Web

--- Zust/Abstract/IMediaService.cs
+++ Zust/Abstract/IMediaService.cs
@@ -1,8 +1,22 @@
 ﻿namespace Zust.Web.Abstract
 {
+    /// <summary>
+    /// Interface for managing media-related operations, such as uploading media files and determining if a file is a video.
+    /// </summary>
     public interface IMediaService
     {
+        /// <summary>
+        /// Uploads a media file asynchronously to the cloud storage service.
+        /// </summary>
+        /// <param name="file">The media file to be uploaded.</param>
+        /// <returns>The URL of the uploaded media file.</returns>
         Task<string> UploadMediaAsync(IFormFile file);
+
+        /// <summary>
+        /// Determines if a media file is a video.
+        /// </summary>
+        /// <param name="mediaFile">The media file to check.</param>
+        /// <returns>True if the file is a video, false otherwise.</returns>
         bool IsVideoFile(IFormFile mediaFile);
     }
 }
--- Zust/Abstract/IStaticService.cs
+++ Zust/Abstract/IStaticService.cs
@@ -1,14 +1,48 @@
 ﻿using Zust.Entities.Models;
 using Zust.Web.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace Zust.Web.Abstract
 {
+    /// <summary>
+    /// Interface for managing static content-related operations, such as retrieving random cover images, status image paths, watch videos, advertisements, and special users.
+    /// </summary>
     public interface IStaticService
     {
+        /// <summary>
+        /// Retrieves a random cover image URL from the specified path.
+        /// </summary>
+        /// <param name="path">The file path containing the list of cover image URLs.</param>
+        /// <returns>A random cover image URL.</returns>
         string GetRandomCoverImage(string path);
+
+        /// <summary>
+        /// Retrieves a list of random status image paths from the specified path.
+        /// </summary>
+        /// <param name="count">The number of random status image paths to retrieve.</param>
+        /// <param name="path">The file path containing the list of status image URLs.</param>
+        /// <returns>A list of random status image paths.</returns>
         List<string> GetRandomStatusImagePaths(int count, string path);
+
+        /// <summary>
+        /// Retrieves a list of watch videos from the specified path.
+        /// </summary>
+        /// <param name="path">The file path containing the list of watch videos.</param>
+        /// <returns>A list of watch videos.</returns>
         List<Video> GetWatchVideos(string path);
+
+        /// <summary>
+        /// Retrieves a list of advertisements from the specified path.
+        /// </summary>
+        /// <param name="path">The file path containing the list of advertisements.</param>
+        /// <returns>A list of advertisements.</returns>
         List<Advertisement> GetAdvertisements(string path);
+
+        /// <summary>
+        /// Retrieves a list of special users asynchronously.
+        /// </summary>
+        /// <returns>A list of special users.</returns>
         Task<List<User>> GetSpecialUsersAsync();
     }
 }
