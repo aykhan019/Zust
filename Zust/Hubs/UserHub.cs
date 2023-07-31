08ae7ae539ed3e9d9cb8741aae623e44df2cfe54
Aykhan <ayxanx17@gmail.com>
2023-07-31 19:22:28 +0400

    Added comments to UserHub

--- Zust/Hubs/UserHub.cs
+++ Zust/Hubs/UserHub.cs
@@ -6,22 +6,41 @@ using Zust.Web.Models;
 
 namespace Zust.Web.Hubs
 {
+    /// <summary>
+    /// SignalR hub for handling real-time communication with connected users.
+    /// </summary>
     public class UserHub : Hub
     {
         // There are repetitions . . .
 
+        /// <summary>
+        /// The service used for user-related operations.
+        /// </summary>
         private readonly IUserService _userService;
 
+        /// <summary>
+        /// Initializes a new instance of the UserHub class with the required dependencies.
+        /// </summary>
+        /// <param name="userService">The service used for user-related operations.</param>
         public UserHub(IUserService userService)
         {
             _userService = userService;
         }
 
+        /// <summary>
+        /// Sends a message to a specific user.
+        /// </summary>
+        /// <param name="message">The message to be sent.</param>
         public async Task SendMessageToUser(Message message)
         {
             await Clients.Users(new string[] { message.ReceiverUserId }).SendAsync(SignalRConstants.ReceiveMessage, message);
         }
 
+        /// <summary>
+        /// Sends a notification and friend request response to a specific user.
+        /// </summary>
+        /// <param name="notification">The notification to be sent.</param>
+        /// <param name="friendRequestViewModel">The view model containing friend request data.</param>
         public async Task SendNotification(Notification notification, FriendRequestViewModel friendRequestViewModel)
         {
             await Clients.Users(new string[] { notification.ToUserId }).SendAsync(SignalRConstants.ReceiveNotification, notification);
@@ -29,16 +48,28 @@ namespace Zust.Web.Hubs
             await Clients.Users(new string[] { notification.ToUserId }).SendAsync(SignalRConstants.ReceiveFriendRequestResponse, friendRequestViewModel);
         }
 
+        /// <summary>
+        /// Sends a comment notification to a specific user.
+        /// </summary>
+        /// <param name="notification">The notification to be sent.</param>
         public async Task SendCommentNotification(Notification notification)
         {
             await Clients.Users(new string[] { notification.ToUserId }).SendAsync(SignalRConstants.ReceiveNotification, notification);
         }
 
+        /// <summary>
+        /// Sends a friend request to a specific user.
+        /// </summary>
+        /// <param name="friendRequest">The friend request to be sent.</param>
         public async Task SendFriendRequest(FriendRequest friendRequest)
         {
             await Clients.Users(new string[] { friendRequest.ReceiverId }).SendAsync(SignalRConstants.ReceiveFriendRequest, friendRequest);
         }
 
+        /// <summary>
+        /// Sends a friend request notification to a specific user.
+        /// </summary>
+        /// <param name="notification">The notification to be sent.</param>
         public async Task SendFriendRequestNotification(Notification notification)
         {
             await Clients.Users(new string[] { notification.ToUserId }).SendAsync(SignalRConstants.ReceiveNotification, notification);
